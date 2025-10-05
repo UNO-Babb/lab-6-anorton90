8c4d0e0d381d3224ae2490f5afa0e76fa4dfbded
anorton90 <alyssianorton@unomaha.edu>
2025-10-05 19:43:16 +0000

    lab 6 done!

--- WordCount.py
+++ WordCount.py
@@ -4,10 +4,23 @@
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
+  textFile = open("fish.txt", 'r')
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
+
   for line in textFile:
-    print(line)
+    lineCount = lineCount + 1
+    words = line.split()
+    for w in words:
+      wordCount = wordCount + 1
+    for l in w:
+      letterCount = letterCount + 1  
+    #print(line)
+
+    print("Lines: ", lineCount)
+    print("Words:", wordCount)
+    print("Letters:", letterCount)
   
 
 if __name__ == '__main__':
